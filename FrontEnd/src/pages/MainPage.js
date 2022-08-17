081f7753bfaa8f7aca5c2771effc69018bb2a329
AhmedAbdelerhman <102978109+AhmedAbdelerhman@users.noreply.github.com>
2022-08-17 10:39:41 +0200

    update postman file update routes

--- FrontEnd/src/pages/MainPage.js
+++ FrontEnd/src/pages/MainPage.js
@@ -1,15 +1,27 @@
-import { Fragment, useState } from "react";
+import { Fragment, useEffect, useState } from "react";
+import { useHistory, useLocation } from "react-router-dom";
 import AllUrlCards from "../components/AllUrlCards";
 import UrlsDialog from "../components/UrlsDialog";
 
 const CardUrl = (props) => {
   const [showDialog, setShowDialog] = useState(false);
+  const location = useLocation()
+  console.log(location.pathname)
   let button = {
     background: "#3f51b5",
     color: "#fff",
     right: "25%",
     top: "2%",
   };
+  const history = useHistory();
+  useEffect(() => {
+    const userInfo = JSON.parse(localStorage.getItem("userInfo"));
+    if (!(userInfo?.userData.token) ) 
+ {   console.log("hi")
+  
+  
+  history.push("/login");}
+  }, [history,location]);
   const showDialogHandler = () => {
     setShowDialog((showDialog) => (showDialog = !showDialog));
   };
@@ -28,7 +40,7 @@ const CardUrl = (props) => {
         onClick={showDialogHandler}
       >
         <span className="btn-label">
-          <i className="bi bi-plus-circle"></i> add url
+          <i className="bi bi-plus-circle"></i> Add Shorten Link 
         </span>
       </button>
     </Fragment>
